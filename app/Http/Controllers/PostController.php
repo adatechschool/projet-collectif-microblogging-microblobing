202d458b5c8635d7dc720547f0e50ad46c9a0254
LiseQuemener <lquemener@hotmail.fr>
2024-04-09 16:44:11 +0200

    création de la route, du controller et affichage d'un post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,9 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
+use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
+
 
 class PostController extends Controller
 {
     //
+    public function show($id): View
+    {
+    $post = Post::findOrFail($id);
+    return view('post', ['post' => $post]);
+}
 }

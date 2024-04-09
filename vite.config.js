c17c37d96361ede3fb00082895f32d665aef4d50
Pierre Pedrono <146751236+PierrePedrono@users.noreply.github.com>
2024-04-09 11:40:45 +0200

    redirection du port vite ->5174

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+
+    server: {
+        port:5174
+    },
     plugins: [
         laravel({
             input: [
